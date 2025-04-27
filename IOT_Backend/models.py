c5f370207de5a53b98760bca1f92d2788c34b1d2
HarshBamotra <harshbamotra12@gmail.com>
2025-04-27 17:09:10 +0530

    Added post method to insert data into DB and other minor changes

--- IOT_Backend/models.py
+++ IOT_Backend/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 
 class SensorData(models.Model):
-    mote_id = models.CharField(max_length=256)
+    node_id = models.CharField(max_length=256)
     timestamp = models.DateTimeField()
-    location = models.CharField(max_length=256)
-    temperature = models.FloatField(default=0.0)
-    gas_concentration = models.FloatField(default=0.0)
+    pm2_5 = models.FloatField(default=0.0)
+    pm10 = models.FloatField(default=0.0)
+    NO = models.FloatField(default=0.0)
+    NO2 = models.FloatField(default=0.0)
+    NOX = models.FloatField(default=0.0)
+    NH3 = models.FloatField(default=0.0)
+    CO = models.FloatField(default=0.0)
+    SO2 = models.FloatField(default=0.0)
+    O3 = models.FloatField(default=0.0)
+    Benzene = models.FloatField(default=0.0)
+    Toluene = models.FloatField(default=0.0)
+    Xylene = models.FloatField(default=0.0)
+    AQI = models.FloatField(default=0.0)
     added_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         indexes = [
-            models.Index(fields=['mote_id', 'timestamp'], name='mote_id'),
+            models.Index(fields=['node_id', 'timestamp'], name='node_id'),
         ]
